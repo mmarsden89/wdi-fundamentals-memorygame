59defb8b8b31962e873656002eab24c949d7f8a7
Matt Marsden <mmarsden89@gmail.com>
2019-01-16 01:31:47 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
 var cards = ["Queen", "Queen", "King", "King"]
 var cardsInPlay = []
-var cardOne = cards[0]
-var cardTwo = cards[0]
 
-cardsInPlay.push(cardOne)
-cardsInPlay.push(cardTwo)
-
-if (cardsInPlay.length === 2) {
-}	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
+var checkforMatch = function() {
+	if (cardsInPlay.length === 2) {
+}		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!")
 	
-}	else {
-		alert("Sorry, try again.")
+}		else {
+			alert("Sorry, try again.")
+}
 }
 
-console.log("User flipped queen");
-console.log("User flipped king")
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+	checkforMatch()
+}
+
+flipCard(0);
+flipCard(1);
