fdf4ab466a16b2bb29a027d2656753007e896816
Matt Marsden <GrassyKnolls@Grassy-Knolls.local>
2019-01-15 22:44:37 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
-console.log("Up and running!");
+var cards = ["Queen", "Queen", "King", "King"]
+var cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[0]
 
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+if (cardsInPlay.length === 2) {
+}	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	
+}	else {
+		alert("Sorry, try again.")
+}
+
+console.log("User flipped queen");
+console.log("User flipped king")
