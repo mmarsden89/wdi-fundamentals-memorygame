db145936ab25faec4a37d9fda0b82c8b7bd58711
Matt Marsden <mmarsden89@gmail.com>
2019-01-16 22:39:29 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["Queen", "Queen", "King", "King"]
+var cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = []
 
 var checkforMatch = function() {
@@ -9,13 +30,15 @@ var checkforMatch = function() {
 }		else {
 			alert("Sorry, try again.")
 }
-}
+};
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId])
+	cardsInPlay.push(cards[cardId].rank)
+	console.log("User flipped " + cards[cardId].rank)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 	checkforMatch()
-}
+};
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
