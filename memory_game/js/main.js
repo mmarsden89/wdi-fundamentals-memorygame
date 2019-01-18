2a2a61dc0ecde1cd109e2f6a145a01cab34dc826
Matt Marsden <mmarsden89@gmail.com>
2019-01-17 22:21:34 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,22 +23,36 @@ var cards = [
 var cardsInPlay = []
 
 var checkforMatch = function() {
-	if (cardsInPlay.length === 2) {
-}		if (cardsInPlay[0] === cardsInPlay[1]) {
+	//if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!")
 	
-}		else {
+}	else {
 			alert("Sorry, try again.")
 }
 };
 
 var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId].rank)
-	console.log("User flipped " + cards[cardId].rank)
-	console.log(cards[cardId].cardImage)
-	console.log(cards[cardId].suit)
-	checkforMatch()
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkforMatch();
+	}
 };
 
-flipCard(0);
-flipCard(2);
+
+console.log(cardsInPlay);
+
+function createBoard() {
+	const cardTable = document.getElementById('game-board');
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		cardTable.appendChild(cardElement);
+	}
+};
+
+createBoard();
